c56851316ac823f4c314ebae442d3cb6a8998c53
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2024-10-02 15:42:44 +0200

    added cname and stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://siphosenkosindhlovu.github.io",
-  base: "/jt",
+  site: "jacarandatechnology.co.zw",
 });
