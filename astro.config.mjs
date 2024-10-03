2a060470d342f4bcab56074707c8febfb5543e79
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2024-10-03 08:50:22 +0200

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "jacarandatechnology.co.zw",
+  site: "https://jacarandatechnology.co.zw",
 });
